b85500bcb1ea6eab2142017718deb6e6b6f1047c
uxeeCoder <umar.afar@aol.com>
2024-04-01 22:47:02 -0400

    Corrected the typo in h1

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function about() {
   return (
     <section>
-<h1>The era of IT</h1>
+<h1>The Era of IT</h1>
         <div>This page is about the age of information technology</div>
     </section>
   )
